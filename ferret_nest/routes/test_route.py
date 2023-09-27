ac6db7098486194edf6ec12f73c07ba440d03de2
Jerry Wu <jerrywu.hr@gmail.com>
2023-09-26 20:47:14 -0700

    routing git status

--- ferret_nest/routes/test_route.py
+++ ferret_nest/routes/test_route.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, Flask
+)
 
 app = Flask(__name__)
 
+bp = Blueprint('test_route', __name__, url_prefix='/test')
 
-@app.route("/")
+
+@bp.route("/hello")
 def hello_world():
-    return "<p>Hello, World!!!</p>"
+    return "<p>routedHello</p>"
