d2f4d84f5ecbc876474d0694a007daa0508eb47b
Jerry Wu <jerrywu.hr@gmail.com>
2023-10-01 16:22:29 -0700

    switching to litestar

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-import os
-from fastapi import Depends, FastAPI, HTTPException, Request
-from jose import JWTError, jwt
-from routers import test
-# from dotenv import load_dotenv
-
-# # Load .env file (if you're using one)
-# load_dotenv()
-
-JWT_SECRET = os.getenv("SUPABASE_JWT_SECRET")
-ALGORITHM = os.getenv("SUPABASE_JWT_ALGORITHM")
+from litestar import Litestar, get
 
 
-def get_current_user(request: Request):
-    token = request.headers.get("Authorization")
-    if not token:
-        raise HTTPException(
-            status_code=401, detail="Authorization header missing")
+@get("/")
+async def index() -> str:
+    return "Hello, world!"
 
-    token = token.split("Bearer ")[-1]
-    try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
-        return payload
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid JWT token")
-
-
-app = FastAPI(dependencies=[Depends(get_current_user)])
-
-app.include_router(test.router)
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+app = Litestar([index])
